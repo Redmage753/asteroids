bcb3f3c8c030d86ae8e06d9393db61e2f7255109
redmage753 <firehazard159@gmail.com>
2025-06-24 09:49:28 -0500

    Working player triangle created.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
-from circleshape import *
+from circleshape import CircleShape
+from player import Player
 
 def main():
     print(f"Starting Asteroids!")
@@ -10,12 +11,18 @@ def main():
     screen=pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
     dt=0 
     clock=pygame.time.Clock()
+
+    x=SCREEN_WIDTH/2
+    y=SCREEN_HEIGHT/2
+    player_radius=PLAYER_RADIUS
+    player=Player(x,y,player_radius)
     quit=False
     while quit == False:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
     dt=clock.tick(60)/1000
 if __name__ == "__main__":
