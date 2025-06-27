ab8d40a97e73b68ae1c33e5e1d20104f712000e5
redmage753 <firehazard159@gmail.com>
2025-06-26 23:37:55 -0500

    updated asteroid split into one line

--- asteroid.py
+++ asteroid.py
@@ -18,24 +18,33 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += (self.velocity * dt)
 
+#    def split(self, dt):
+#        self.kill()
+#        if self.radius <= ASTEROID_MIN_RADIUS:
+#            return
+#        else:
+#            split_angle_pos = random.uniform(20,50)
+#            split_angle_neg = random.uniform(20,50)*-1
+#            #print(f"{split_asteroid1}")
+#            #print(f"{split_angle_neg}")
+#            #print(f"{split_angle_pos}")
+#            new_radius = self.radius - ASTEROID_MIN_RADIUS
+#            split_asteroid1 = Asteroid(self.position.x,self.position.y,new_radius)
+#            #split_asteroid1.velocity = (pygame.Vector2(0,1).rotate(split_asteroid1.rotation) + split_angle_pos) * 1.2 
+#            split_asteroid1.velocity = self.velocity.rotate(split_angle_pos) * 1.2
+#            #split_asteroid1.velocity = dt * 1.2
+#            split_asteroid2 = Asteroid(self.position.x,self.position.y,new_radius)
+#            #split_asteroid2.rotation += (split_angle_neg)
+#            split_asteroid2.velocity = self.velocity.rotate(split_angle_neg) * 1.2
+#            #split_asteroid2.velocity = (pygame.Vector2(0,1).rotate(split_asteroid2.rotation) + split_angle_neg) * 1.2
+#            #print(type(split_asteroid1.position), type(split_asteroid1.velocity), type(dt))
+##            #print(type(split_asteroid2.position), type(split_asteroid2.velocity), type(dt))
     def split(self, dt):
         self.kill()
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
         else:
-            split_angle_pos = random.uniform(20,50)
-            split_angle_neg = random.uniform(20,50)*-1
-            #print(f"{split_asteroid1}")
-            #print(f"{split_angle_neg}")
-            #print(f"{split_angle_pos}")
-            new_radius = self.radius - ASTEROID_MIN_RADIUS
-            split_asteroid1 = Asteroid(self.position.x,self.position.y,new_radius)
-            #split_asteroid1.velocity = (pygame.Vector2(0,1).rotate(split_asteroid1.rotation) + split_angle_pos) * 1.2 
-            split_asteroid1.velocity = self.velocity.rotate(split_angle_pos) * 1.2
-            #split_asteroid1.velocity = dt * 1.2
-            split_asteroid2 = Asteroid(self.position.x,self.position.y,new_radius)
-            #split_asteroid2.rotation += (split_angle_neg)
-            split_asteroid2.velocity = self.velocity.rotate(split_angle_neg) * 1.2
-            #split_asteroid2.velocity = (pygame.Vector2(0,1).rotate(split_asteroid2.rotation) + split_angle_neg) * 1.2
-            #print(type(split_asteroid1.position), type(split_asteroid1.velocity), type(dt))
-            #print(type(split_asteroid2.position), type(split_asteroid2.velocity), type(dt))
+            
+            Asteroid(self.position.x,self.position.y,(self.radius-ASTEROID_MIN_RADIUS)).velocity = self.velocity.rotate(random.uniform(20,50))
+            asteroid = Asteroid(self.position.x,self.position.y,(self.radius-ASTEROID_MIN_RADIUS))
+            asteroid.velocity = self.velocity.rotate(random.uniform(20,50)*-1)
